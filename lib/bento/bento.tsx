b5cd88afe84dfbe1899593937e7dd382cd9fbf02
monodyle <monodylh@gmail.com>
2023-02-22 21:51:31 +0700

    fix responsive (temporary)

--- lib/bento/bento.tsx
+++ lib/bento/bento.tsx
@@ -1,3 +1,4 @@
+import cx from 'classnames'
 import { ComponentWithChildren } from '../types/types'
 
 type Props = {
@@ -8,7 +9,14 @@ export const Bento: ComponentWithChildren<Props> = ({ children, title }) => {
   return (
     <div class="mb-8">
       {title && <div class="p-6 text-lg font-semibold">{title}</div>}
-      <div class="grid auto-rows-fr xl:gap-10 gap-6 grid-rows-[repeat(auto-fill,minmax(178px,1fr))] grid-cols-[repeat(2,178px)] xl:grid-cols-[repeat(4,175px)] xl:grid-rows-[repeat(auto-fill,minmax(175px,1fr))]">
+      <div
+        class={cx(
+          'grid auto-rows-fr',
+          'gap-6 grid-rows-[repeat(auto-fill,minmax(151.5px,1fr))] grid-cols-[repeat(2,151.5px)]',
+          'md:grid-rows-[repeat(auto-fill,minmax(178px,1fr))] md:grid-cols-[repeat(2,178px)]',
+          'xl:gap-10 xl:grid-cols-[repeat(4,175px)] xl:grid-rows-[repeat(auto-fill,minmax(175px,1fr))]'
+        )}
+      >
         {children}
       </div>
     </div>
--- lib/profile/profile.tsx
+++ lib/profile/profile.tsx
@@ -2,7 +2,7 @@ import avatar from '#/assets/avatar.png'
 
 export const Profile = () => {
   return (
-    <header class="flex items-center gap-8">
+    <header class="flex items-start gap-8 flex-col md:flex-row md:items-center">
       <div class="aspect-square rounded-full overflow-hidden relative w-32 h-32">
         <img src={avatar} class="block absolute inset-0 object-cover" />
       </div>
